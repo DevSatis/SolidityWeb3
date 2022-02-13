877b4c527c12577a88c90cc4dea6ab5b286ea4ef
Satish Kumar <sksatishk9@gmail.com>
2022-02-13 21:56:16 +0530

    Constructor changes.

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -25,7 +25,7 @@ contract TodoList{
   );
 
   constructor() public {
-    createTask("Check out dappuniversity.com");
+    createTask("Check out at my git Account");
   }
 
   function createTask(string memory _content) public {
